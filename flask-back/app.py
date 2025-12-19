383ef01c86f29286fbeea7c444bcaeee83be3fe1
Yash gaikwad <gaikwadyash964@gmail.com>
2025-12-20 00:13:00 +0530

    second

--- flask-back/app.py
+++ flask-back/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Flask Backend Running Successfully",
+        "message": "Flask Backend Running ",
         "status": "OK"
     })
 
